63ab7b7aad75ba42eba0b22a2173370f17bb8f0d
Valentin Lorentz <progval@gmail.com>
2013-09-16 09:18:39 +0200

    Add tests runner.

---
+++ Test/run.sh
@@ -0,0 +1,1 @@
+python -m unittest Test.BenchmarkingMARS88 Test.BenchmarkingMARS_94nop Test.Core Test.Instruction88 Test.MARS88 Test.ProcessQueue Test.Calculator Test.Instruction Test.Parser Test.Warrior
