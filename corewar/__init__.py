5ed87b83b666e63a7567812ac0d1fb9d4cd2c40e
Barkley Vowk <vowkalg@gmail.com>
2019-02-08 10:04:25 -0800

    Rework the __repr__ for warriors to match previous output more closely

--- corewar/__init__.py
+++ corewar/__init__.py
@@ -85,10 +85,12 @@ class Warrior(object):
 
     def __repr__(self):
         """Return string representation of warrior"""
+        s = ''
 
-        s  = 'Program "%s" ' % self.name
-        s += '(length %d) ' % len(self.instructions)
-        s += 'by "%s"\n\n' % self.author
+        s += ';redcode-%s\n'
+        s += ';name %s\n'
+        s += ';author %s\n'
+        s += ';assert 1\n'
 
         if not self.standard == STANDARD_88:
             s += '       ORG      START\n'
